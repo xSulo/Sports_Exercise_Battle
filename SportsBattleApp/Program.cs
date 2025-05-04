dff6d6ae42c92562aa7a4ebf9a156fdd1274c722
Süleyman Tegmen <stegmen@iastate.edu>
2025-05-04 04:58:17 +0200

    Added the very basics for tcp

--- SportsBattleApp/Program.cs
+++ SportsBattleApp/Program.cs
@@ -1,12 +1,18 @@
 ﻿using SportsBattleApp.Http;
-using DatabaseConnection = SportsBattleApp.Data.DatabaseConnection;
+using SportsBattleApp.Tcp;
 
+using DatabaseConnection = SportsBattleApp.Data.DatabaseConnection;
 DatabaseConnection dbConnection;
 dbConnection = new DatabaseConnection();
 /*dbConnection.OpenConnection();
 dbConnection.CloseConnection();
 dbConnection.Dispose();*/
 
+var tcpServer = new TcpTournament();
+await tcpServer.StartAsync();
+
 var router = new RequestRouter();
 var httpServer1 = new HttpServer(router);
 await httpServer1.StartAsync();
+
+
